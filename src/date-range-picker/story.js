414f8195118f4f5f4ccb0c98008b36d3ed6ed5ad
Nabyl Bennouri <nabyl.bennouri@density.io>
2021-05-17 15:57:11 -0700

    Add storybook story for NO_HEADER calendar.

--- src/date-range-picker/story.js
+++ src/date-range-picker/story.js
@@ -1,294 +1,359 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import "./styles.module.scss";
 
-import './styles.module.scss';
-import DateRangePicker, { DateRangePickerContext } from './index';
+import DateRangePicker, { DateRangePickerContext } from "./index";
 
-import moment from 'moment';
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import moment from "moment";
+import { storiesOf } from "@storybook/react";
 
 // used in multiple stories
 const commonRanges = [
   {
-    id: 'WEEK_TO_DATE',
-    name: 'Week to date',
-    startDate: moment().startOf('week'),
+    id: "WEEK_TO_DATE",
+    name: "Week to date",
+    startDate: moment().startOf("week"),
+    endDate: moment(),
+  },
+  {
+    id: "MONTH_TO_DATE",
+    name: "Month to date",
+    startDate: moment().startOf("month"),
+    endDate: moment(),
+  },
+  {
+    id: "QUARTER_TO_DATE",
+    name: "Quarter to date",
+    startDate: moment().startOf("quarter"),
+    endDate: moment(),
+  },
+  {
+    id: "LAST_WEEK",
+    name: "Last week",
+    startDate: moment()
+      .subtract(1, "week")
+      .startOf("week"),
     endDate: moment()
+      .subtract(1, "week")
+      .endOf("week"),
   },
   {
-    id: 'MONTH_TO_DATE',
-    name: 'Month to date',
-    startDate: moment().startOf('month'),
+    id: "LAST_MONTH",
+    name: "Last month",
+    startDate: moment()
+      .subtract(1, "month")
+      .startOf("month"),
     endDate: moment()
+      .subtract(1, "month")
+      .endOf("month"),
   },
   {
-    id: 'QUARTER_TO_DATE',
-    name: 'Quarter to date',
-    startDate: moment().startOf('quarter'),
+    id: "LAST_QUARTER",
+    name: "Last quarter",
+    startDate: moment()
+      .subtract(1, "quarter")
+      .startOf("quarter"),
     endDate: moment()
+      .subtract(1, "quarter")
+      .endOf("quarter"),
   },
   {
-    id: 'LAST_WEEK',
-    name: 'Last week',
-    startDate: moment().subtract(1, 'week').startOf('week'),
-    endDate: moment().subtract(1, 'week').endOf('week')
+    id: "LAST_7_DAYS",
+    name: "Last 7 days",
+    startDate: moment().subtract(1, "week"),
+    endDate: moment(),
   },
   {
-    id: 'LAST_MONTH',
-    name: 'Last month',
-    startDate: moment().subtract(1, 'month').startOf('month'),
-    endDate: moment().subtract(1, 'month').endOf('month')
+    id: "LAST_30_DAYS",
+    name: "Last 30 days",
+    startDate: moment().subtract(1, "month"),
+    endDate: moment(),
   },
   {
-    id: 'LAST_QUARTER',
-    name: 'Last quarter',
-    startDate: moment().subtract(1, 'quarter').startOf('quarter'),
-    endDate: moment().subtract(1, 'quarter').endOf('quarter')
-  },
-  {
-    id: 'LAST_7_DAYS',
-    name: 'Last 7 days',
-    startDate: moment().subtract(1, 'week'),
-    endDate: moment()
-  },
-  {
-    id: 'LAST_30_DAYS',
-    name: 'Last 30 days',
-    startDate: moment().subtract(1, 'month'),
-    endDate: moment()
-  },
-  {
-    id: 'LAST_90_DAYS',
-    name: 'Last 90 days',
-    startDate: moment().subtract(1, 'quarter'),
-    endDate: moment()
+    id: "LAST_90_DAYS",
+    name: "Last 90 days",
+    startDate: moment().subtract(1, "quarter"),
+    endDate: moment(),
   },
 ];
 
-storiesOf('DateRangePicker', module)
-  .add('Open, start date selected', () => (
+storiesOf("DateRangePicker", module)
+  .add("Open, start date selected", () => (
     <DateRangePicker
-      onChange={action('dates change')}
-      onFocusChange={action('focus')}
+      onChange={action("dates change")}
+      onFocusChange={action("focus")}
       focusedInput="startDate"
       startDate={moment()}
-      endDate={moment().subtract(1, 'day')}
+      endDate={moment().subtract(1, "day")}
     />
   ))
-  .add('With common date ranges', () => {
-    return <DateRangePicker
-      onChange={action('dates change')}
-      onFocusChange={action('focus')}
-      focusedInput="startDate"
-      startDate={moment()}
-      endDate={moment().subtract(1, 'day')}
-      commonRanges={commonRanges}
-      onSelectCommonRange={action('common range selected')}
-    />;
-  })
-  .add('Floated right', () => (
-    <div style={{paddingLeft: 500, width: 400}}>
+  .add("With common date ranges", () => {
+    return (
       <DateRangePicker
-        anchor="ANCHOR_RIGHT"
-        onChange={action('dates change')}
-        onFocusChange={action('focus')}
+        onChange={action("dates change")}
+        onFocusChange={action("focus")}
         focusedInput="startDate"
         startDate={moment()}
-        endDate={moment().subtract(1, 'day')}
+        endDate={moment().subtract(1, "day")}
+        commonRanges={commonRanges}
+        onSelectCommonRange={action("common range selected")}
+      />
+    );
+  })
+  .add("Floated right", () => (
+    <div style={{ paddingLeft: 500, width: 400 }}>
+      <DateRangePicker
+        anchor="ANCHOR_RIGHT"
+        onChange={action("dates change")}
+        onFocusChange={action("focus")}
+        focusedInput="startDate"
+        startDate={moment()}
+        endDate={moment().subtract(1, "day")}
       />
     </div>
   ))
-  .add('Floated right with common date ranges', () => {
-    return <div style={{paddingLeft: 500, width: 400}}>
-      <DateRangePicker
-        anchor="ANCHOR_RIGHT"
-        onChange={action('dates change')}
-        onFocusChange={action('focus')}
-        focusedInput="startDate"
-        startDate={moment()}
-        endDate={moment().subtract(1, 'day')}
-        commonRanges={commonRanges}
-        onSelectCommonRange={action('common range selected')}
-      />
-    </div>
-  })
-  .add('Interactive', () => {
-    class Wrapper extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          focus: null,
-          startDate: moment(),
-          endDate: moment().add(1, 'day'),
-        };
-      }
-      render() {
-        return <DateRangePicker
-          startDate={this.state.startDate}
-          endDate={this.state.endDate}
-          onChange={e => this.setState({startDate: e.startDate, endDate: e.endDate})}
-
-          focusedInput={this.state.focus}
-          onFocusChange={focus => this.setState({focus})}
-          isOutsideRange={day => moment(day).diff(moment()) > 999999999}
-        />;
-      }
-    }
-
-    return <Wrapper />;
-  })
-  .add('Interactive with auto-close', () => {
-    class Wrapper extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          focus: null,
-          startDate: moment(),
-          endDate: moment().add(1, 'day'),
-        };
-      }
-      render() {
-        return <DateRangePicker
-          autoClose={true}
-          startDate={this.state.startDate}
-          endDate={this.state.endDate}
-          onChange={e => this.setState({startDate: e.startDate, endDate: e.endDate})}
-
-          focusedInput={this.state.focus}
-          onFocusChange={focus => this.setState({focus})}
-        />;
-      }
-    }
-
-    return <Wrapper />;
-  })
-  .add('Interactive with common date ranges', () => {
-    class Wrapper extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          startDate: moment(),
-          endDate: moment().add(1, 'day'),
-        };
-      }
-      render() {
-        return <DateRangePicker
-          startDate={this.state.startDate}
-          endDate={this.state.endDate}
-          onChange={e => this.setState({ startDate: e.startDate, endDate: e.endDate })}
+  .add("Floated right with common date ranges", () => {
+    return (
+      <div style={{ paddingLeft: 500, width: 400 }}>
+        <DateRangePicker
+          anchor="ANCHOR_RIGHT"
+          onChange={action("dates change")}
+          onFocusChange={action("focus")}
+          focusedInput="startDate"
+          startDate={moment()}
+          endDate={moment().subtract(1, "day")}
           commonRanges={commonRanges}
-          onSelectCommonRange={commonRange => {
-            this.setState({
-              startDate: commonRange.startDate,
-              endDate: commonRange.endDate
-            })
-          }}
-          onOpenCommonRangeList={action('common range list opened')}
-        />;
-      }
-    }
-
-    return <Wrapper />;
+          onSelectCommonRange={action("common range selected")}
+        />
+      </div>
+    );
   })
-  .add('Interactive with common date ranges and subtitles', () => {
+  .add("Interactive", () => {
     class Wrapper extends React.Component {
       constructor(props) {
         super(props);
         this.state = {
           focus: null,
           startDate: moment(),
-          endDate: moment().add(1, 'day'),
+          endDate: moment().add(1, "day"),
         };
       }
       render() {
-        return <DateRangePicker
-          startDate={this.state.startDate}
-          endDate={this.state.endDate}
-          onChange={e => this.setState({ startDate: e.startDate, endDate: e.endDate })}
-          focusedInput={this.state.focus}
-          onFocusChange={focus => this.setState({ focus })}
-          commonRanges={commonRanges}
-          showCommonRangeSubtitles={true}
-          onSelectCommonRange={commonRange => {
-            this.setState({
-              startDate: commonRange.startDate,
-              endDate: commonRange.endDate
-            })
-          }}
-          onOpenCommonRangeList={action('common range list opened')}
-        />;
+        return (
+          <DateRangePicker
+            startDate={this.state.startDate}
+            endDate={this.state.endDate}
+            onChange={(e) =>
+              this.setState({ startDate: e.startDate, endDate: e.endDate })
+            }
+            focusedInput={this.state.focus}
+            onFocusChange={(focus) => this.setState({ focus })}
+            isOutsideRange={(day) => moment(day).diff(moment()) > 999999999}
+          />
+        );
       }
     }
 
     return <Wrapper />;
   })
-  .add('Interactive and not floating', () => {
-    class Wrapper extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          startDate: moment(),
-          endDate: moment().add(1, 'day'),
-        };
-      }
-      render() {
-        return <DateRangePicker
-          startDate={this.state.startDate}
-          endDate={this.state.endDate}
-          floating={false}
-          onChange={e => this.setState({ startDate: e.startDate, endDate: e.endDate })}
-          commonRanges={commonRanges}
-          onSelectCommonRange={commonRange => {
-            this.setState({
-              startDate: commonRange.startDate,
-              endDate: commonRange.endDate
-            })
-          }}
-          onOpenCommonRangeList={action('common range list opened')}
-        />;
-      }
-    }
-
-    return <Wrapper />;
-  })
-  .add('With TIME_RANGE_CONTROL_BAR context', () => {
+  .add("Interactive with auto-close", () => {
     class Wrapper extends React.Component {
       constructor(props) {
         super(props);
         this.state = {
           focus: null,
           startDate: moment(),
-          endDate: moment().add(1, 'day'),
+          endDate: moment().add(1, "day"),
         };
       }
       render() {
-        return <DateRangePickerContext.Provider value="TIME_RANGE_CONTROL_BAR">
+        return (
           <DateRangePicker
             autoClose={true}
             startDate={this.state.startDate}
             endDate={this.state.endDate}
-            onChange={e => this.setState({startDate: e.startDate, endDate: e.endDate})}
-
+            onChange={(e) =>
+              this.setState({ startDate: e.startDate, endDate: e.endDate })
+            }
             focusedInput={this.state.focus}
-            onFocusChange={focus => this.setState({focus})}
+            onFocusChange={(focus) => this.setState({ focus })}
           />
-        </DateRangePickerContext.Provider>;
+        );
       }
     }
 
     return <Wrapper />;
   })
-  .add('With deprecated SMALL_WIDTH context', () => (
+  .add("Interactive with common date ranges", () => {
+    class Wrapper extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          startDate: moment(),
+          endDate: moment().add(1, "day"),
+        };
+      }
+      render() {
+        return (
+          <DateRangePicker
+            startDate={this.state.startDate}
+            endDate={this.state.endDate}
+            onChange={(e) =>
+              this.setState({ startDate: e.startDate, endDate: e.endDate })
+            }
+            commonRanges={commonRanges}
+            onSelectCommonRange={(commonRange) => {
+              this.setState({
+                startDate: commonRange.startDate,
+                endDate: commonRange.endDate,
+              });
+            }}
+            onOpenCommonRangeList={action("common range list opened")}
+          />
+        );
+      }
+    }
+
+    return <Wrapper />;
+  })
+  .add("Interactive with common date ranges and subtitles", () => {
+    class Wrapper extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          focus: null,
+          startDate: moment(),
+          endDate: moment().add(1, "day"),
+        };
+      }
+      render() {
+        return (
+          <DateRangePicker
+            startDate={this.state.startDate}
+            endDate={this.state.endDate}
+            onChange={(e) =>
+              this.setState({ startDate: e.startDate, endDate: e.endDate })
+            }
+            focusedInput={this.state.focus}
+            onFocusChange={(focus) => this.setState({ focus })}
+            commonRanges={commonRanges}
+            showCommonRangeSubtitles={true}
+            onSelectCommonRange={(commonRange) => {
+              this.setState({
+                startDate: commonRange.startDate,
+                endDate: commonRange.endDate,
+              });
+            }}
+            onOpenCommonRangeList={action("common range list opened")}
+          />
+        );
+      }
+    }
+
+    return <Wrapper />;
+  })
+  .add("Interactive and not floating", () => {
+    class Wrapper extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          startDate: moment(),
+          endDate: moment().add(1, "day"),
+        };
+      }
+      render() {
+        return (
+          <DateRangePicker
+            startDate={this.state.startDate}
+            endDate={this.state.endDate}
+            floating={false}
+            onChange={(e) =>
+              this.setState({ startDate: e.startDate, endDate: e.endDate })
+            }
+            commonRanges={commonRanges}
+            onSelectCommonRange={(commonRange) => {
+              this.setState({
+                startDate: commonRange.startDate,
+                endDate: commonRange.endDate,
+              });
+            }}
+            onOpenCommonRangeList={action("common range list opened")}
+          />
+        );
+      }
+    }
+
+    return <Wrapper />;
+  })
+  .add("With TIME_RANGE_CONTROL_BAR context", () => {
+    class Wrapper extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          focus: null,
+          startDate: moment(),
+          endDate: moment().add(1, "day"),
+        };
+      }
+      render() {
+        return (
+          <DateRangePickerContext.Provider value="TIME_RANGE_CONTROL_BAR">
+            <DateRangePicker
+              autoClose={true}
+              startDate={this.state.startDate}
+              endDate={this.state.endDate}
+              onChange={(e) =>
+                this.setState({ startDate: e.startDate, endDate: e.endDate })
+              }
+              focusedInput={this.state.focus}
+              onFocusChange={(focus) => this.setState({ focus })}
+            />
+          </DateRangePickerContext.Provider>
+        );
+      }
+    }
+
+    return <Wrapper />;
+  })
+  .add("With NO_HEADER context", () => {
+    class Wrapper extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          focus: null,
+          startDate: moment(),
+          endDate: moment().add(1, "day"),
+        };
+      }
+      render() {
+        return (
+          <DateRangePickerContext.Provider value="NO_HEADER">
+            <DateRangePicker
+              onChange={action("dates change")}
+              onFocusChange={action("focus")}
+              focusedInput="startDate"
+              startDate={moment()}
+              numberOfMonths={1}
+              endDate={moment().add(3, "day")}
+            />
+          </DateRangePickerContext.Provider>
+        );
+      }
+    }
+
+    return <Wrapper />;
+  })
+  .add("With deprecated SMALL_WIDTH context", () => (
     <DateRangePickerContext.Provider value="SMALL_WIDTH">
       <DateRangePicker
-        onChange={action('dates change')}
-        onFocusChange={action('focus')}
+        onChange={action("dates change")}
+        onFocusChange={action("focus")}
         focusedInput="startDate"
         startDate={moment()}
-        endDate={moment().subtract(1, 'day')}
+        endDate={moment().subtract(1, "day")}
         numberOfMonths={1}
         commonRanges={commonRanges}
-        onSelectCommonRange={action('common range selected')}
+        onSelectCommonRange={action("common range selected")}
       />
     </DateRangePickerContext.Provider>
-  ))
+  ));
