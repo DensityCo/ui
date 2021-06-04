93c3226f08cf461c0fefb5f34a4ff031076a4a9a
Gus Cost <guscost@gmail.com>
2021-06-04 17:24:23 -0400

    code: clean up story

--- src/tooltip/story.js
+++ src/tooltip/story.js
@@ -1,11 +1,8 @@
 import React, { Fragment } from 'react';
 import { storiesOf } from '@storybook/react';
-import Tooltip from './index';
-
-import Card, { CardHeader, CardBody } from '../card/index';
-import fontVariables from '../../variables/fonts.json';
-
 import Icons from '../icons';
+import Tooltip from './index';
+import fontVariables from '../../variables/fonts.json';
 
 
 storiesOf('Tooltip', module)
@@ -35,26 +32,6 @@ storiesOf('Tooltip', module)
           </Fragment>}
         />
       </span>
-    </div>
-  ))
-  .add('In a header of a card', () => (
-    <div style={{width: 800}}>
-      <Card>
-        <CardHeader>
-          My header with quite a long name&nbsp;
-          <Tooltip
-            target={<div><Icons.Info /></div>}
-            contents={<Fragment>
-              <h3>Utilization</h3>
-              <p>A measure of lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla diam orci, lacinia ac vestibulum ut, vehicula at eros. Pellentesque molestie eu odio nec volutpat. Fusce cursus sapien quis massa tincidunt pellentesque. Sed molestie orci a augue auctor iaculis. Donec pharetra fringilla sem in convallis. Maecenas diam nisi, hendrerit rhoncus aliquam et, bibendum et felis. Nunc nec tortor interdum, mattis lectus nec, aliquet lectus. Vivamus hendrerit pharetra metus ut pulvinar.</p>
-              <br/>
-            </Fragment>}
-          />
-        </CardHeader>
-        <CardBody>
-          foo bar baz
-        </CardBody>
-      </Card>
     </div>
   ))
   .add('With a custom target', () => (
